2352eb56806451e461b94b0c50df6410bc635afe
soso01 <6125024@gmail.com>
2021-09-29 01:22:49 +0900

    <feat> translate 구글 api 연동

--- src/constants/text.js
+++ src/constants/text.js
@@ -3,6 +3,8 @@ export const TEXT = {
   scope1: {
     scope2: {
       message: '스코프 1, 2',
+      message2: '메세지 1, 2',
     },
   },
+  bye: '굿 바이',
 };
---
+++ utils/googleTranslate.js
@@ -0,0 +1,48 @@
+const { Translate } = require('@google-cloud/translate').v2;
+const fs = require('fs');
+const path = require('path');
+
+import { LANGUAGES, TEXT } from '@constants';
+import { googleTranslateKey } from './key';
+
+// Instantiates a client
+const translate = new Translate({ key: googleTranslateKey });
+
+(async function run() {
+  saveLocaleJson();
+  saveLocaleIndexJs();
+})();
+
+function saveLocaleIndexJs() {
+  fs.writeFileSync(
+    path.resolve(__dirname, '../src/i18n/locales/index.js'),
+    LANGUAGES.reduce(
+      (acc, lang) =>
+        acc + `export { default as ${lang} } from './${lang}.json';`,
+      '',
+    ),
+  );
+}
+
+function saveLocaleJson() {
+  LANGUAGES.forEach(async lang => {
+    const result = await translateObj(TEXT, lang);
+    fs.writeFileSync(
+      path.resolve(__dirname, `../src/i18n/locales/${lang}.json`),
+      JSON.stringify(result),
+    );
+  });
+}
+
+async function translateObj(obj, lang) {
+  const result = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    result[key] =
+      typeof value === 'object'
+        ? await translateObj(value, lang)
+        : (await translate.translate(value, lang))[0];
+  }
+
+  return result;
+}
