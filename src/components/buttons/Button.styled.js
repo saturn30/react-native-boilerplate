e7dd4344601c4ab395292b46ced983449788f45a
soso01 <6125024@gmail.com>
2021-10-04 17:39:03 +0900

    <chore> 불필요한 코드 삭제

--- src/components/buttons/Button.styled.js
+++ src/components/buttons/Button.styled.js
@@ -1,7 +1,6 @@
 import { TouchableOpacity, Text as RNText } from 'react-native';
-
 import styled from 'styled-components/native';
-import tw, { getColor } from 'tailwind-rn';
+import tw from 'tailwind-rn';
 
 export const Button = styled(TouchableOpacity)`
   ${tw('p-3 bg-blue-300 rounded-lg w-40')};
